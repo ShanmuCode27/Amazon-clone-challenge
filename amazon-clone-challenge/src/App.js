8c746e682398e18e2ea0e88c7de2fa638c8ce4e7
NishCode27 <90558895+NishCode27@users.noreply.github.com>
2022-07-25 22:21:47 +0530

    testing

--- amazon-clone-challenge/src/App.js
+++ amazon-clone-challenge/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi <code>src/App.js</code> and save to reload.
           And this is react native amazon-clone project
         </p>
         <a
