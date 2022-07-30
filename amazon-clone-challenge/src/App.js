c85f36d97042015cb77d8efd7f3beecc23fffa37
enogeston <enogestonstefan9@gmail.com>
2022-07-30 22:03:20 +0530

    Paragraph

--- amazon-clone-challenge/src/App.js
+++ amazon-clone-challenge/src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi <code>src/App.js</code> and save to reload.
-          And this is react native amazon-clone project
+          Hi im shanmu
         </p>
         <a
           className="App-link"
